c89cb8e9bb57d0a7d407f7961ba5ed7432e96c48
Amine karimi <aminekarimi1998@gmail.com>
2022-05-08 19:31:53 +0100

    add paging movie source

--- app/src/main/java/com/amine/tmdb/domain/repository/MovieSource.kt
+++ app/src/main/java/com/amine/tmdb/domain/repository/MovieSource.kt
@@ -15,10 +15,8 @@ class MovieSource(
     private val language: String = "en-US",
     private val includeAdult: Boolean = false
 ) : PagingSource<Int, MovieEntity>() {
+
     override fun getRefreshKey(state: PagingState<Int, MovieEntity>): Int? {
-        // We need to get the previous key (or next key if previous is null) of the page
-        // that was closest to the most recently accessed index.
-        // Anchor position is the most recently accessed index
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
                 ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
@@ -27,7 +25,6 @@ class MovieSource(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieEntity> {
         val position = params.key ?: STARTING_PAGE_INDEX
-        //val apiQuery = query + IN_QUALIFIER
 
         return try {
             val response = service.fetchMoviesList(
@@ -42,18 +39,18 @@ class MovieSource(
             val nextKey = if (repos.isEmpty()) {
                 null
             } else {
-                // initial load size = 3 * NETWORK_PAGE_SIZE
-                // ensure we're not requesting duplicating items, at the 2nd request
-                position + (params.loadSize / 2)
+                position + 1
             }
             LoadResult.Page(
                 data = repos,
-                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
+                prevKey = null,
                 nextKey = nextKey
             )
         } catch (exception: IOException) {
             return LoadResult.Error(exception)
         } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        } catch (exception: Exception) {
             return LoadResult.Error(exception)
         }
     }
